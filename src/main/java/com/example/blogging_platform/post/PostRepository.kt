c02d4d3294c3d532e5269297aeafc8f2d9259e6a
Seongyoon Park <57535543+Pokko653@users.noreply.github.com>
2025-02-10 11:15:02 +0900

    Revert "Migrate Java to Kotlin"

--- src/main/java/com/example/blogging_platform/post/PostRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.blogging_platform.post
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface PostRepository : JpaRepository<Post?, Long?>
--- src/main/java/com/example/blogging_platform/writer/WriterRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.blogging_platform.writer
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface WriterRepository : JpaRepository<Writer?, Long?>
